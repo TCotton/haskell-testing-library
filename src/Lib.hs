5b4c1b0466ffa131b42838c31a6a8a5d3aff3387
Andy Walpole <andy.walpole@gmail.com>
2021-05-03 10:15:20 +0100

    added basic functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,39 @@ isPalindrome :: String -> Bool
 isPalindrome text = cleanText == reverse cleanText
   where cleanText = filter (not . (`elem` ['!','.'])) text
 
+-- "this is a string"
+isString :: String -> String
+isString x = x ::String
+
+-- 'z'
+isChar :: Char -> Char
+isChar x = x ::Char
+
+-- true
+isBool :: Bool -> Bool
+isBool x = x ::Bool
+
+-- 1
+isInt :: Int -> Int
+isInt x = x ::Int
+
+-- 30414093201713378043612608166064768844377641568960512000000000000
+isInteger :: Integer -> Integer
+isInteger x = x ::Integer
+
+-- 4.0
+isFloat :: Float -> Float
+isFloat x = x ::Float
+
+
+
+
+
+
+
+
+
+
 
 
 
